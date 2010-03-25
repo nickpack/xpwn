ceff24a45b4aa584749b0d9f488a94c4a8757a1a
Joshua Hill <posixninja@gmail.com>
2010-03-25 00:29:42 -0400

    fixed ipsw to allow the use of pwnagetool bundles

--- ipsw-patch/main.c
+++ ipsw-patch/main.c
@@ -431,7 +431,27 @@ int main(int argc, char* argv[]) {
 	ramdiskVolume = openVolume(ramdiskFS);
 	XLOG(0, "growing ramdisk: %d -> %d\n", ramdiskVolume->volumeHeader->totalBlocks * ramdiskVolume->volumeHeader->blockSize, (ramdiskVolume->volumeHeader->totalBlocks + 4) * ramdiskVolume->volumeHeader->blockSize);
 	grow_hfs(ramdiskVolume, (ramdiskVolume->volumeHeader->totalBlocks + 4) * ramdiskVolume->volumeHeader->blockSize);
-
+	
+	/* Apply Ramdisk Patches */
+	XLOG(0, "patching ramdisk\n");
+	firmwarePatches = (Dictionary*)getValueByKey(info, "RamdiskPatches");
+	patchDict = (Dictionary*) firmwarePatches->values;
+	while(patchDict != NULL) {
+		fileValue = (StringValue*) getValueByKey(patchDict, "File");
+					
+		patchValue = (StringValue*) getValueByKey(patchDict, "Patch");
+		patchPath = (char*) malloc(sizeof(char) * (strlen(bundlePath) + strlen(patchValue->value) + 2));
+		strcpy(patchPath, bundlePath);
+		strcat(patchPath, "/");
+		strcat(patchPath, patchValue->value);
+				
+		XLOG(0, "patching %s (%s)... ", fileValue->value, patchPath);
+		doPatchInPlace(ramdiskVolume, fileValue->value, patchPath);
+		free(patchPath);
+		
+		patchDict = (Dictionary*) patchDict->dValue.next;
+	}
+	
 	if(doBootNeuter) {
 		firmwarePatches = (Dictionary*)getValueByKey(info, "BasebandPatches");
 		if(firmwarePatches != NULL) {
